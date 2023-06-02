f89799c9dca101f5785080691c4a112c256131db
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-06-02 09:32:50 +0300

    Add files via upload

---
+++ src/Destination components/Destination.js
@@ -0,0 +1,10 @@
+import DestinationBody from "./DestinationBody"
+
+function Destination(){
+    return(
+        <div>
+            <DestinationBody/>
+        </div>
+    )
+}
+export default Destination
---
+++ src/Destination components/Europa.jsx
@@ -0,0 +1,16 @@
+import React, { useRef } from 'react'
+import moon from "../space-tourism-website-main/starter-code/assets/destination/Europa.webp"
+import * as THREE from 'three';
+import { useGLTF } from '@react-three/drei'
+export default function Model(props) {
+  const { nodes, materials } = useGLTF('/scene-transformed.glb')
+  const material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(moon) });
+  material.color = new THREE.Color("#9b9283");
+  return (
+    <group {...props} dispose={null}>
+    <mesh geometry={nodes.Sphere_Material002_0.geometry} material={material} rotation={[-Math.PI / 2, 0, 0]} scale={100} />
+  </group>
+  )
+}
+
+useGLTF.preload('/scene-transformed.glb')
